a8ddfb3368e0fbd309e24ec0491cc736547832d6
k-bai <kongbaiyo@live.cn>
2019-02-09 20:20:17 +0800

    test

--- data/sheep/functions/sheep_effect/ice/ice2.py
+++ data/sheep/functions/sheep_effect/ice/ice2.py
@@ -1,9 +1,9 @@
 import math
 
-def qiumian_to_zhijiao(r, phi, omega):
-	z = r*math.cos(omega)
-	x = r*math.cos(omega)*math.cos(phi)
-	y = r*math.cos(omega)*math.sin(phi)
+def qiumian_to_zhijiao(r, phi, theta):
+	z = r*math.cos(theta)
+	x = r*math.cos(theta)*math.cos(phi)
+	y = r*math.cos(theta)*math.sin(phi)
 	return (x, y, z)
 
 command = 'particle minecraft:dust 0 1 1 1 ~{:.3f} ~{:.3f} ~{:.3f} 0 0 0 1 1 normal'
@@ -12,5 +12,5 @@ radius = 4
 
 for i in range(parts):
 	for j in range(parts):
-		cood = qiumian_to_zhijiao(radius, 2*math.pi/parts*i, 2*math.pi/parts*j)
+		cood = qiumian_to_zhijiao(radius, 2*math.pi/parts*i, math.pi/parts*j)
 		print(command.format(cood[0], cood[1], cood[2]))
