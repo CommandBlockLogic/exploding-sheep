814558e14a4e5870c1f74da85d6aa36a8ddedf41
k-bai <kongbaiyo@live.cn>
2019-02-09 20:11:50 +0800

    test

--- data/sheep/functions/sheep_effect/ice/ice2.py
+++ data/sheep/functions/sheep_effect/ice/ice2.py
@@ -12,5 +12,5 @@ radius = 4
 
 for i in range(parts):
 	for j in range(parts):
-		cood = qiumian_to_zhijiao(radius, 2*math.pi/parts*i, math.pi/parts*j)
+		cood = qiumian_to_zhijiao(radius, 2*math.pi/parts*i, 2*math.pi/parts*j)
 		print(command.format(cood[0], cood[1], cood[2]))
