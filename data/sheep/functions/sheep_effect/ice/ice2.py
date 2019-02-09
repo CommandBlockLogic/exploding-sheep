30bff08a1d0fe8d82ad5b5ef6f257b8c8b070068
k-bai <kongbaiyo@live.cn>
2019-02-09 20:10:11 +0800

    test

--- data/sheep/functions/sheep_effect/ice/ice2.py
+++ data/sheep/functions/sheep_effect/ice/ice2.py
@@ -10,29 +10,7 @@ command = 'particle minecraft:dust 0 1 1 1 ~{:.3f} ~{:.3f} ~{:.3f} 0 0 0 1 1 nor
 parts = 20
 radius = 4
 
-
-# 最大弧长 点之间间隔
-arc_len = 2*math.pi*radius/parts
-
-omega_start = -math.pi/2
-omega_stop = math.pi/2
-omega_step = math.pi/parts
-
-omega = omega_start
-while omega <= omega_stop:
-	# 当前半径
-	current_radius = radius*math.cos(omega)
-	# 最大弧长在这个切面中的个数
-	current_parts = math.ceil(2*math.pi*current_radius/arc_len)
-	if current_parts == 0:
-		current_parts = 1
-	phi_start = 0
-	phi_stop = 2*math.pi
-	phi_step = 2*math.pi/current_parts
-	phi = phi_start
-	while phi <= phi_stop:
-		cood = qiumian_to_zhijiao(radius, phi, omega)
+for i in range(parts):
+	for j in range(parts):
+		cood = qiumian_to_zhijiao(radius, 2*math.pi/parts*i, math.pi/parts*j)
 		print(command.format(cood[0], cood[1], cood[2]))
-		phi += phi_step
-	omega += omega_step
-
