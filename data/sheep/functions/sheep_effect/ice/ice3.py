98eb041d33a4d12ec823a8aa2e00c787e1c78999
k-bai <kongbaiyo@live.cn>
2019-02-09 20:33:10 +0800

    test

--- data/sheep/functions/sheep_effect/ice/ice3.py
+++ data/sheep/functions/sheep_effect/ice/ice3.py
@@ -2,14 +2,15 @@ import math
 import random
 
 command = 'particle minecraft:dust 0 1 1 1 ~{:.3f} ~{:.3f} ~{:.3f} 0 0 0 1 1 normal'
+radius = 4
 
 for i in range(100):
 	r2 = 1
 	while r2 >= 1 :
-		u = random.random()
-		v = random.random()
+		u = random.random()*2-1
+		v = random.random()*2-1
 		r2 = u**2+v**2
-	x = 2*u*math.sqrt(1-r2)
-	y = 2*v*math.sqrt(1-r2)
-	z = 1-2*r2
+	x = radius*2*u*math.sqrt(1-r2)
+	y = radius*2*v*math.sqrt(1-r2)
+	z = radius*1-2*r2
 	print(command.format(x, y, z))
