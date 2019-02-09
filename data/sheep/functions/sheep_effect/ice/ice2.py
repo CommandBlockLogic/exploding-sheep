42898e13f8bf82d7297c101abdfc71ad2c67641a
k-bai <kongbaiyo@live.cn>
2019-02-09 20:03:26 +0800

    test

--- data/sheep/functions/sheep_effect/ice/ice2.py
+++ data/sheep/functions/sheep_effect/ice/ice2.py
@@ -7,26 +7,32 @@ def qiumian_to_zhijiao(r, phi, omega):
 	return (x, y, z)
 
 command = 'particle minecraft:dust 0 1 1 1 ~{:.3f} ~{:.3f} ~{:.3f} 0 0 0 1 1 normal'
-dots = 20
+parts = 20
 radius = 4
+
+
 # 最大弧长 点之间间隔
-arc_len = 2*math.pi*radius/dots
+arc_len = 2*math.pi*radius/parts
 
+omega_start = -math.pi/2
+omega_stop = math.pi/2
+omega_step = math.pi/parts
 
-
-for i in range(dots+1):
-	# 最大弧长对应平面角
-	angle = 2*math.pi/dots * i
-	# 当前切面对应半径
-	current_radius = abs(math.sin(angle)*radius)
-	# 当前可分弧段数
-	parts = math.ceil(2*math.pi*current_radius/arc_len)
-	if parts == 0:
-		parts = 1
-	# 当前弧段对应角度
-	current_angle = 2*math.pi/parts
-	# 循环
-	for j in range(parts):
-		# 直角坐标
-		cood = qiumian_to_zhijiao(radius, 2*math.pi/parts*j, angle)
+omega = omega_start
+while omega <= omega_stop:
+	# 当前半径
+	current_radius = radius*math.cos(omega)
+	# 最大弧长在这个切面中的个数
+	current_parts = math.ceil(2*math.pi*current_radius/arc_len)
+	if current_parts == 0:
+		current_parts = 1
+	phi_start = 0
+	phi_stop = 2*math.pi
+	phi_step = 2*math.pi/current_parts
+	phi = phi_start
+	while phi <= phi_stop:
+		cood = qiumian_to_zhijiao(radius, phi, omega)
 		print(command.format(cood[0], cood[1], cood[2]))
+		phi += phi_step
+	omega += omega_step
+
