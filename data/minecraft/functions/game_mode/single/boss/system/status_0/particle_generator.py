86341ec7eef729d780bca596c0e067b89de04d92
夏可文 <xiakewen@HerobrineXia.local>
2019-02-15 01:00:16 +0800

    Fix particle bugs-Try:2

--- data/minecraft/functions/game_mode/single/boss/system/status_0/particle_generator.py
+++ data/minecraft/functions/game_mode/single/boss/system/status_0/particle_generator.py
@@ -3,7 +3,7 @@ import math
 
 part = 50
 r = 5
-format_sentence = "particle minecraft:flame ~{:.3f} 13 {:.3f} 0.0001 0.0001 0.0001 0.001 1 force @a"
+format_sentence = "particle minecraft:flame ~{:.3f} 13 ~{:.3f} 0.0001 0.0001 0.0001 0.001 1 force @a"
 
 for i in range(part):
 	print(format_sentence.format(math.cos(part/180*math.pi*i)*r,math.sin(part/180*math.pi*i)*r))
